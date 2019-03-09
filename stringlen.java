dd2d02ab03480defeba8308e005b3db050c031c7
Rajasekar V <47376271+Raj252@users.noreply.github.com>
2019-03-09 15:21:16 +0530

    Create stringlen.java

---
+++ stringlen.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Stringlen 
+{
+public static void main(String[] args) 
+{
+Scanner sc = new Scanner(System.in);
+ 
+String str= sc.nextLine();
+int count=0;
+String array[]= str.split(" ",0);
+ 
+ for(int i=0;i<array.length;i++){count+=array[i].length();}
+System.out.println(count);
+}
+}
